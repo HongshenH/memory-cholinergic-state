15f34d7e7a4d8cdc49896011f676eb0bcaf2283d
Hongshen He <89462778+HongshenH@users.noreply.github.com>
2026-06-26 19:46:23 +0900

    Add files via upload

---
+++ 01_fig1_septal_circuit/CellSorter-master/CellSorter-master/@CellSorter/kcluster.m
@@ -0,0 +1,14 @@
+% performs k-means clustering on a 2-D manifold
+
+% Arguments:
+%   Y: the data as an N x 2 matrix
+% Outputs:
+%
+
+function labels = kcluster(self, Y)
+
+  % perform k-means clustering into three classes
+%   labels = kmeans(Y, self.nClusters, 'Distance', 'sqeuclidean', 'Start', 'plus', 'Replicates', 3, 'EmptyAction', 'error', 'Options', self.sset);
+  labels = kmeans(Y, self.nClusters, 'Distance', 'sqeuclidean', 'Start', 'plus', 'Replicates', 50, 'EmptyAction', 'error', 'Options', self.sset);
+
+end % function

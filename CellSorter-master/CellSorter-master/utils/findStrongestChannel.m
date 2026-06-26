e614a7cb81bd3122fb7c26d4409a9dc7907c67ab
Hongshen He <89462778+HongshenH@users.noreply.github.com>
2026-06-26 20:09:15 +0900

    Delete CellSorter-master/CellSorter-master directory

--- CellSorter-master/CellSorter-master/utils/findStrongestChannel.m
+++
@@ -1,6 +0,0 @@
-function channel_index = findStrongestChannel(x)
-  % x is a 50x4 matrix, where the first index is the timeseries and the second is the channel number
-
-  [~, channel_index] = max(max(x));
-
-end % function
